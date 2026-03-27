e9eb76b62fa073dddbbea6a31ec5d5084a403cec
Hedinsei <Hedinsei@gmail.com>
2026-03-27 10:21:54 -0700

    feat: implement decks and cards schema; update main function to retrieve decks; add security rule

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,33 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
-export const usersTable = pgTable("users", {
+export const decksTable = pgTable("decks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  userId: varchar("user_id", { length: 255 }).notNull(),
+  title: varchar({ length: 255 }).notNull(),
+  description: text(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const decksRelations = relations(decksTable, ({ many }) => ({
+  cards: many(cardsTable),
+}));
+
+export const cardsTable = pgTable("cards", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  deckId: integer("deck_id")
+    .notNull()
+    .references(() => decksTable.id, { onDelete: "cascade" }),
+  front: text().notNull(),
+  back: text().notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const cardsRelations = relations(cardsTable, ({ one }) => ({
+  deck: one(decksTable, {
+    fields: [cardsTable.deckId],
+    references: [decksTable.id],
+  }),
+}));
